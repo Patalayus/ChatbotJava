1dae9f768f40846033f853339433a76ad0687201
patalayus <lukecharles222@gmail.com>
2020-06-26 12:20:05 +0100

    inital commit

---
+++ chatbot.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.lang.Math;
+//class name is same as file name
+
+public class chatbot{
+    public static void main(String args[]){
+        //scanner initialised
+        Scanner inpt = new Scanner(System.in);
+        System.out.println("Hello and welcome to the 2020 Summer Chatbot\nPlease enter your name:");
+        String userName = inpt.nextLine();
+        System.out.println("Hello, "+userName+" how are you?");
+        String userMood = inpt.nextLine();
+        if(userMood.equals("good")){
+            System.out.println("I'm good too");
+        }else{
+            System.out.println("ah alright");
+        }
+        //root question
+        System.out.println("What would you like to do today?\n1. Mathematics\n2. English\n3. Science");
+        double userChoice0 = inpt.nextDouble();
+
+        //class object declaration
+        numbers numObj = new numbers();
+
+
+        if(userChoice0 == 1){
+            System.out.println("Maths it is!");
+            numObj.method1();
+            
+        }else if(userChoice0 == 2){
+            System.out.println("English it is!");
+        }else if(userChoice0 == 3){
+            System.out.println("Science it is!");
+        }
+        
+    }
+}
---
+++ numbers.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class numbers{
+    static void method1(){
+    }
+}
