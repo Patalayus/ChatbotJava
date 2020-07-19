29c68c1bd0eb80663030516fef6a8c4a5e56f286
patalayus <lukecharles222@gmail.com>
2020-07-20 00:29:07 +0100

    spelling fixes

--- letters.java
+++ letters.java
@@ -4,17 +4,17 @@ public class letters {
     static void method2(){
         chatbot jump = new chatbot();
 
-        System.out.println("What would you like to do?\n1. Vowels\n2. Consenants\n3. Nouns\n4. Adjective");
+        System.out.println("What would you like to do?\n1. Vowels\n2. Consenants\n3. Nouns\n4. Adjectives");
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
         if(choice == 1){
-
+            System.out.println("You have chosen vowels...");
         }else if(choice == 2){
-
+            System.out.println("You have chosen Consenants");
         }else if(choice == 3){
-
+            System.out.println("You have chosen Nouns");
         }else if(choice == 4){
-
+            System.out.println("You have chosen Adjectives");
         }
     }
 }
