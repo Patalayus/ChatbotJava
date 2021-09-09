821ff3d3cb1a74f320b60d33bbdf781a570149ca
patalayus <lukecharles222@gmail.com>
2021-09-09 19:34:45 +0100

    facts added, and tested

--- quickfire.java
+++ quickfire.java
@@ -1,27 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
-public class quickfire {
+public class quickfire extends chatbot {
 
     void quickfire(){
-        //quickfire method is used for random questions from any topic
+        //quickfire method is used for random facts
+        Scanner scan = new Scanner(System.in);
+        int x = 0;
+        System.out.println("Be prepared for random facts! Press Q to finish!");
+        chatbot quiObj2 = new chatbot();
 
         List<String> messages = Arrays.asList(
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8",
-                "9",
-                "10"
+                "Oranges aren't naturally occurring fruits.",
+                "The majority of your brain is fat.",
+                "Some cats are actually allergic to humans.",
+                "'OMG' usage can be traced back to 1917.",
+                "A chef's hat has exactly 100 pleats.",
+                "Competitive art used to be in the Olympics.",
+                "High heels were originally worn by men.",
+                "Stop signs used to be yellow.",
+                "New York was briefly named 'New Orange.'",
+                "There was a successful Tinder match in Antarctica in 2014."
 
         );
-        System.out.println(messages.get(random()));
+        do{
+            System.out.println(messages.get(random()));
+            String input_ = scan.nextLine();
+            if(input_.equals("q") || input_.equals("Q")){
+                x++;
+            }
+        }while(x!=1);
+
     }
 
     int random(){
